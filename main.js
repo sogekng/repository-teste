bb6f7483477e23087947e2e405da98fbe74b9140
sogekng <gutsantos3@gmail.com>
2022-08-04 09:27:39 -0300

    Arrumando estruturas

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 function showFooterOne(){
-    var footer_one = document.getElementById('footer');
+    var footer_one = document.getElementById('footer_one');
     var footer_two = document.getElementById('footer_two');
 
     footer_one.style.transitionDelay = "all 0.5s";
     footer_two.style.transitionDelay = "all 0.5s";
 
     if(footer_one.style.opacity == "0" || footer_one.style.opacity == ""){
+        footer_two.style.transform = "translateX(0)";
         footer_two.style.opacity = "0";
+        footer_one.style.transform = "translateX(0px)";
         footer_one.style.opacity = "1";
     }
     
 }
 
 function showFooterTwo(){
-    var footer_one = document.getElementById('footer');
+    var footer_one = document.getElementById('footer_one');
     var footer_two = document.getElementById('footer_two');
 
     footer_one.style.transitionDelay = "all 0.5s";
     footer_two.style.transitionDelay = "all 0.5s";
 
     if(footer_two.style.opacity == "0" || footer_two.style.opacity == ""){
+        footer_one.style.transform = "translateX(0)";
         footer_one.style.opacity = "0";
+        footer_two.style.transform = "translateX(0px)";
         footer_two.style.opacity = "1";
     }
 }
