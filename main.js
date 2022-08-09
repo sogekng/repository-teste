65fdf30ff5d9f81a8745bca309f573afc963f630
sogekng <gutsantos3@gmail.com>
2022-08-09 13:17:34 -0300

    update

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ function sobre(num){
     console.log(num)
     if(num == 1){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }else if(num == 2){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }else if(num == 3){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }else if(num == 4){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }else if(num == 5){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }else if(num == 6){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }else if(num == 7){
         window.location.href = "curso_detalhes.html";
+        console.log(num)
     }
 }
