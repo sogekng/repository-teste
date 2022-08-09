c3d66834a1e3020c7a0d076c02ac12d92466229b
sogekng <gutsantos3@gmail.com>
2022-08-09 11:29:35 -0300

    update

---
+++ enviar-email.php
@@ -0,0 +1,36 @@
+<?php
+  //Variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $contato = $_POST['telefone'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  echo "Nome: $nome <br>";
+  echo "E-mail: $email <br>";
+  echo "Contato: $contato <br>";
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Contato: </b>$contato</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "gutsantos3@gmail.com";
+  $assunto = "Contato pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+?>
