847270b6277a125d050b0edbad950f1f14f46808
sogekng <gutsantos3@gmail.com>
2022-08-08 22:04:11 -0300

    update

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ function sobre(num){
 
     console.log(num)
     if(num == 1){
-        document.getElementById("categoria").innerHTML = "Categoria: <b>Celulares</b>";
         window.location.href = "curso_detalhes.html";
     }else if(num == 2){
         window.location.href = "curso_detalhes.html";
-        categoria.innerHTML = "Curso 2";
     }
 }
