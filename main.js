1f7af87f4d8aa57162ded095f79c73725802ae54
sogekng <gutsantos3@gmail.com>
2022-08-12 22:58:32 -0300

    update files

--- main.js
+++ main.js
@@ -65,3 +65,24 @@ function doSomething(scrollPos) {
 //    ticking = true;
 //  }
 //});
+
+function mudarContentSobre(){
+  document.getElementById('content-1').style.backgroundColor = 'rgb(249, 205, 72)';
+  document.getElementById('content-2').style.backgroundColor = '#2e3033';
+  document.getElementById('sobre').style.width = '100%';
+  document.getElementById('contato').style.width = '0%';
+  document.getElementById('contato-content').style.opacity = '0';
+  document.getElementById('contato-content').style.display = 'none';
+  document.getElementById('footer').style.backgroundColor = '#2e3033';
+  document.getElementById('footer').style.color = 'rgba(255, 255, 255, .9)';                    
+  
+}
+
+function mudarContentContato(){
+  document.getElementById('contato-content').style.opacity = '1';
+  document.getElementById('contato-content').style.display = 'flex';
+  document.getElementById('sobre').style.width = '0%';
+  document.getElementById('contato').style.width = '100%';
+  document.getElementById('footer').style.backgroundColor = 'rgb(249, 205, 72)';
+  document.getElementById('footer').style.color = '#2e3033';
+}
