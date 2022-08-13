b58c7b99994ceb0bdd9b78e2eed9ff94a655d6f5
sogekng <gutsantos3@gmail.com>
2022-08-13 00:20:39 -0300

    update files

--- enviar-email.php
+++ enviar-email.php
@@ -16,6 +16,8 @@ $email = $_POST['email'];
 $telefone = $_POST['telefone'];
 $curso = $_POST['curso'];
 
+$from = 'gutsantos3@gmail.com';
+
 try{
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
@@ -25,31 +27,57 @@ try{
     $mail->Password   = 'lopaejvotfwtcldw';
     $mail->Port = 587;
 
-    $mail->setFrom('gutsantos3@gmail.com');
-    $mail->addAddress('gutsantos3@gmail.com');
+    $mail->setFrom($from);
+    $mail->addAddress($from);
 
     $mail->isHTML(true);
     $mail->Subject = 'Here is the subject';
     $mail->Body    = 
-        '<div style="background-color:brown;">
-            <p">
-                Nome: '.$nome.'
-                <br>
-                E-mail: '.$email.'
-                <br>
-                Telefone: '.$telefone.'
-                <br>
-                Tipo de curso: '.$curso.'
-            </p>
-        </div>';
+        '
+        <table width="100%" height="100%" cellspacing="20" cellpadding="0" border="0" bgcolor="#eaeaeb" style="background-color:#eaeaeb">
+            <tbody><tr>
+                <td align="center" valign="middle">
+                    <font align="center" color="#0" face="Verdana, Arial, sans-serif" size="1" style="padding-bottom:5px;display:block;line-height:16px"></font>
+                    <table width="800" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="background-color:#ffffff" align="center">
+                        <tbody><tr>
+                        </tr>
+                        <tr>
+                            <td bgcolor="yellow" width="800" height="180" valign="middle" style="text-align:center">
+                                <div><span style="font-family:"Myriad Pro",Arial,sans-serif;font-size:38px;display:block">Confirmação de Envio de formulario<br></span></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td valign="middle" style="text-align:center">
+                                <table width="100%" cellpadding="0" cellspacing="45" border="0">
+                                    <tbody><tr>
+                                        <td>
+                                            <span style="font-family:"Myriad Pro",Arial,sans-serif;font-size:16px;color:#494949;display:block">
+                                                
+                                                <span style="font-size:30px;display:block">Olá '.$from.' mais uma pessoa peencheu o formulario do curso</span>
+                                                <br><span style="display:flex;align-items:center;font-size:1rem;">Seu Nome é '.$nome.'</span>
+                                                <br><span style="display:flex;align-items:center;font-size:1rem;">Seu E-mail é '.$email.'</span>
+                                                <br><span style="display:flex;align-items:center;font-size:1rem;">Seu Numero de telefone é '.$telefone.'</span>
+                                                <br><span style="display:flex;align-items:center;font-size:1rem;">Seu Tipo de curso é '.$curso.'</span>
+                                                <br><span style="display:flex;align-items:center;font-size:1,2rem;"><a href="http://api.whatsapp.com/send?1=pt_BR&phone=55'.$telefone.'">Entre em contanto com ele pelo o whatsapp</a></span>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tbody></table>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                </td>
+            </tr>
+        </tbody></table>
+        </body>';
 
     if($nome == '' || $email == '' || $telefone == '' || $curso == ''){
         echo '<script>alert("Campos vazios!");</script>';
-        header("location: index.html");
+        header("location: ./index.html");
     } else {
         if($mail->send()){
             echo 'Email enviado com sucesso';
-            header("location: index.html");
+            header("location: ./index.html");
         }else{ 
             echo 'Erro ao enviar o email';
         }
